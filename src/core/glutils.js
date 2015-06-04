ede755d583ff344c964a870e203c1aa6db2c1c45
Vladimir Klepov <v.klepov@gmail.com>
2015-06-04 12:47:01 +0400

    Frankensteination

---
+++ src/core/glutils.js
@@ -0,0 +1,107 @@
+'use strict';
+	
+(function(global) {
+	var _G = global.grafar,
+		pool = _G.pool,
+        isExisty = _G.isExisty,
+        Panel = _G.Panel,
+        config = _G.config,
+		
+		_T = global.THREE,
+		Object3D = _T.Object3D,
+		PointCloud = _T.PointCloud,
+		Line = _T.Line,
+		LinePieces = _T.LinePieces,
+		BufferGeometry = _T.BufferGeometry,
+		BufferAttribute = _T.BufferAttribute,
+        
+        PointCloudMaterial = _T.PointCloudMaterial,
+        LineBasicMaterial = _T.LineBasicMaterial,
+        MeshLambertMaterial = _T.MeshLambertMaterial,
+        DoubleSide = _T.DoubleSide;
+	
+    
+    function matHelper(type, col) {
+        var mat = null;
+        if (type === 'point')
+            mat = new PointCloudMaterial({
+                size: config.particleRadius, 
+                transparent: true, 
+                opacity: 0.5, 
+                sizeAttenuation: false
+            });
+        else if (type === 'line')
+            mat = new LineBasicMaterial({
+            });            
+        else if (type === 'mesh')
+            mat = new MeshLambertMaterial({
+                side: DoubleSide,
+                transparent: true,
+                opacity: .5,
+                depthWrite: false
+                //depthTest: false
+            });
+        mat.color = col;
+        return mat;
+    };
+    
+    function interleave(tab, names, target) {
+        // need reactive interleave target!
+        // or at least size
+        var itemsize = names.length;
+		for (var j = 0; j < itemsize; j++) {
+			if (isExisty(names[j])) {
+				var colData = tab[names[j]].value(),
+                    len = tab[names[j]].length;
+				for (var i = 0, k = j; i < len; i++, k += itemsize)
+					target[k] = colData[i];
+			}
+		}
+    };
+        
+    function InstanceGL(panel, col) {
+        var pointGeometry = new BufferGeometry(),
+			lineGeometry = new BufferGeometry(),
+			meshGeometry = new BufferGeometry(),
+			position = new BufferAttribute(pool.get(Float32Array, 0), 3),
+			lineIndex = new BufferAttribute(pool.get(Uint32Array, 0), 2),
+			meshIndex = new BufferAttribute(pool.get(Uint32Array, 0), 3),
+			normal = new BufferAttribute(pool.get(Float32Array, 0), 3);
+			
+		pointGeometry.addAttribute('position', position);
+		lineGeometry.addAttribute('position', position);
+		meshGeometry.addAttribute('position', position);
+		lineGeometry.addAttribute('index', lineIndex);
+		meshGeometry.addAttribute('index', meshIndex);
+		meshGeometry.addAttribute('normal', normal);
+		
+		var object = new Object3D();
+        object.add(new PointCloud(pointGeometry, matHelper('point', col)))
+            .add(new Line(lineGeometry, matHelper('line', col), LinePieces))
+            .add(new THREE.Mesh(meshGeometry, matHelper('mesh', col)));
+		panel.scene.add(object);
+        
+        this.panel = panel;
+        this.position = position;
+        this.segments = lineIndex;
+        this.faces = meshIndex;
+        this.normals = normal;
+        this.object = object;
+    };
+    
+    function resizeBuffer(buffer, size) {
+        var oldArr = buffer.array,
+            oldSize = oldArr.length;
+        if (size !== oldSize) {
+            var temp = pool.get(oldArr.constructor, size);
+            temp.set(oldArr.subarray(0, Math.min(oldSize, size)));
+            pool.push(buffer.array);
+            buffer.array = temp;
+        }
+    };
+    
+    
+	_G.InstanceGL = InstanceGL;
+	_G.interleave = interleave;
+	_G.resizeBuffer = resizeBuffer;
+}(this));
--- src/core/reactive_object.js
+++ src/core/reactive_object.js
@@ -1,89 +1,28 @@
 'use strict';
 	
 (function(global) {
-	var _G = global.grafar,
-		_T = global.THREE,
-		
+	var _G = global.grafar,		
 		Database = _G.Database,
 		Style = _G.Style,
 		pool = _G.pool,
         isExisty = _G.isExisty,
-        
+        Graph = _G.GraphR,        
         DatabaseR = _G.DatabaseR,
-		
-		Object3D = _T.Object3D,
-		PointCloud = _T.PointCloud,
-		Line = _T.Line,
-		LinePieces = _T.LinePieces,
-		BufferGeometry = _T.BufferGeometry,
-		BufferAttribute = _T.BufferAttribute;
-	
-    function interleave(tab, names, target) {
-        // need reactive interleave target!
-        // or at least size
-        var itemsize = names.length;
-		for (var j = 0; j < itemsize; j++) {
-			if (isExisty(names[j])) {
-				var colData = tab[names[j]].value(),
-                    len = tab[names[j]].length;
-				for (var i = 0, k = j; i < len; i++, k += itemsize)
-					target[k] = colData[i];
-			}
-		}
-    };
-    
-    
-    function InstanceGL(panel) {
-        var pointGeometry = new BufferGeometry(),
-			lineGeometry = new BufferGeometry(),
-			meshGeometry = new BufferGeometry(),
-			position = new BufferAttribute(pool.get(Float32Array, 0), 3),
-			lineIndex = new BufferAttribute(pool.get(Uint32Array, 0), 2),
-			meshIndex = new BufferAttribute(pool.get(Uint32Array, 0), 3),
-			normal = new BufferAttribute(pool.get(Float32Array, 0), 3);
-			
-		pointGeometry.addAttribute('position', position);
-		lineGeometry.addAttribute('position', position);
-		meshGeometry.addAttribute('position', position);
-		lineGeometry.addAttribute('index', lineIndex);
-		meshGeometry.addAttribute('index', meshIndex);
-		meshGeometry.addAttribute('normal', normal);
-		
-		var col = Style.randColor(),
-            object = new Object3D();
-        object.add(new PointCloud(pointGeometry, Style.matHelper('point', col)))
-            .add(new Line(lineGeometry, Style.matHelper('line', col), LinePieces))
-            .add(new THREE.Mesh(meshGeometry, Style.matHelper('mesh', col)));
-		panel.scene.add(object);
-        
-        this.panel = panel;
-        this.position = position;
-        this.segments = lineIndex;
-        this.faces = meshIndex;
-        this.normals = normal;
-        this.object = object;
-    };
-    
-    function resizeBuffer(buffer, size) {
-        var oldArr = buffer.array,
-            oldSize = oldArr.length;
-        if (size !== oldSize) {
-            var temp = pool.get(oldArr.constructor, size);
-            temp.set(oldArr.subarray(0, Math.min(oldSize, size)));
-            pool.push(buffer.array);
-            buffer.array = temp;
-        }
-    };
-    
+        InstanceGL = _G.InstanceGL,
+        interleave = _G.interleave,
+        resizeBuffer = _G.resizeBuffer;
 	
 	function Object(opts) {
 		this.db = new DatabaseR();
 		this.glinstances = [];
+        this.graphs = [];
 		this.hidden = false;
+        this.col = Style.randColor();
 	}
 		
-	Object.prototype.pin = function(panel) {			
-		this.glinstances.push(new InstanceGL(panel));
+	Object.prototype.pin = function(panel) {
+		this.glinstances.push(new InstanceGL(panel, this.col));
+        this.graphs.push(new Graph());
 		return this;
 	};
 	
