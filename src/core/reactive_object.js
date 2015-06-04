c947044898839ddd437e7e377607e895ec491f92
Vladimir Klepov <v.klepov@gmail.com>
2015-06-04 12:50:27 +0400

    Merge reactive DB into Object

--- src/core/reactive_object.js
+++ src/core/reactive_object.js
@@ -1,19 +1,21 @@
 'use strict';
 	
 (function(global) {
-	var _G = global.grafar,		
-		Database = _G.Database,
+	var _G = global.grafar,
 		Style = _G.Style,
 		pool = _G.pool,
         isExisty = _G.isExisty,
-        Graph = _G.GraphR,        
-        DatabaseR = _G.DatabaseR,
+        Graph = _G.GraphR,
+        asArray = _G.asArray,
+        Reactive = _G.Reactive,
         InstanceGL = _G.InstanceGL,
         interleave = _G.interleave,
         resizeBuffer = _G.resizeBuffer;
 	
+    
 	function Object(opts) {
-		this.db = new DatabaseR();
+		this.reactives = {};
+        
 		this.glinstances = [];
         this.graphs = [];
 		this.hidden = false;
@@ -27,16 +29,59 @@
 	};
 	
 	Object.prototype.constrain = function(constraint) {
-		this.db.constrain(constraint);
+		var names = asArray(constraint.what || []),
+			using = asArray(constraint.using || []),
+			as = constraint.as || function() {},
+			maxlen = constraint.maxlen;
+            
+        if (names.length > 1)
+            throw new Error('cannot define > 1');
+        var sources = [];
+        for (var i = 0; i < using.length; i++) {
+            if (!this.reactives.hasOwnProperty(using[i]))
+                this.reactives[using[i]] = new Reactive();
+            sources[i] = this.reactives[using[i]];
+        }
+        if (!this.reactives.hasOwnProperty(names[0]))
+            this.reactives[names[0]] = new Reactive();
+        
+        var compatibilityAs = function(par, out, l) {
+            out.buffer(par.length === 0? maxlen: par[0].length);
+            var data = {};
+            for (var i = 0; i < using.length; i++)
+                data[using[i]] = par[i].value();
+            data[names[0]] = out.data;
+            as(data, out.length, {});
+        };
+        
+        this.reactives[names[0]]
+            .lift(compatibilityAs)
+            .bind(Reactive.unify(sources));
+
 		return this;
 	};
 	
+    Object.prototype.select = function(names) {
+        var names = asArray(names || []);
+        var temp = [];        
+        for (var i = 0; i < names.length; i++) {
+            if (!this.reactives.hasOwnProperty(names[i]))
+                throw new Error('cannot select undefined');
+            temp[i] = this.reactives[names[i]];
+        }
+		var unifiedReactives = Reactive.unify(temp);
+        var data = {};
+        for (var i = 0; i < names.length; i++)
+            data[names[i]] = unifiedReactives[i].validate();
+        return data;
+	};
+    
 	Object.prototype.refresh = function() {
 		for (var i = 0; i < this.glinstances.length; i++) {
 			var instance = this.glinstances[i],
 				names = instance.panel._axes;
 				
-			var tab = this.db.select(names);
+			var tab = this.select(names);
             resizeBuffer(instance.position, tab[names[0]].length * names.length);
 			interleave(tab, names, instance.position.array);
 			instance.position.needsUpdate = true;
