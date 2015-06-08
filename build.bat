44d16aa7ad6e0bd15f8f53b449ac398805548f09
Vladimir Klepov <v.klepov@gmail.com>
2015-06-08 22:26:06 +0400

    build + example ups

--- build.bat
+++ build.bat
@@ -2,32 +2,41 @@ md build
 cd build
 
 set target=grafar.js
+set temp=temp.js
 copy nul %target% /y
 	
 set srcList=..\src\grafar.js+^
 ..\src\math\polyfills.js+^
 ..\src\math\misc.js+^
-..\src\core\observer.js+^
 ..\src\core\pool.js+^
 ..\src\math\set.js+^
 ..\src\math\vector.js+^
 ..\src\math\numerics.js+^
-..\src\math\graphdata.js+^
+..\src\math\array_utils.js+^
 ..\src\core\style.js+^
-..\src\core\table.js+^
-..\src\core\database.js+^
-..\src\core\object.js+^
-..\src\generators\gen_interface.js+^
-..\src\generators\grafaryaz\planner.js+^
-..\src\generators\grafaryaz\parser.js+^
+..\src\math\graphdata.js+^
+..\src\core\parser_alt.js+^
+..\src\core\reactive.js+^
+..\src\core\glutils.js+^
+..\src\core\reactive_topo.js+^
+..\src\core\reactive_graph.js+^
+..\src\core\reactive_object.js+^
 ..\src\generators\grafaryaz\parser_.js+^
-..\src\generators\grafaryaz\context.js+^
-..\src\core\animation.js+^
 ..\src\core\panel.js+^
-..\src\core\graph.js+^
+..\src\legacy\observer.js+^
+..\src\core\animation.js+^
 ..\src\legacy\grafar_samples.js+^
 ..\src\legacy\grafar_ui.js
 echo %srcList%
-copy /b %srcList% %target%
+copy /b %srcList% %temp%
+
+copy /b ..\libs\i-color.min.js+^
+..\libs\three.min.js+^
+..\libs\Detector.js+^
+..\libs\OrbitControls.js+^
+..\libs\stats.min.js+^
+%temp% %target%
 
 cd ../
+
+PAUSE
