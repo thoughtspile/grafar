fbb95a241a59f403b2ab86a6be9fc12e7eec8335
Vladimir Klepov <v.klepov@gmail.com>
2015-06-11 14:36:48 +0400

    simpler axis logic

--- src/core/fn_utils.js
+++ src/core/fn_utils.js
@@ -18,36 +18,30 @@
     };
         
     var nListMap = function(nargs) {
-        if (!grafar.isExisty(nListMap.memo[nargs])) {
-            var application = '';
-            var getvals = '';//var srcdata = [';
-            for (var i = 0; i < nargs; i++) {
-                //application += 'srcdata[' + i + '][i]';
-                application += 'srcdata_' + i + '[i]';
-                //getvals += 'src[' + i + '].value()';
-                getvals += 'var srcdata_' + i + ' = src[' + i + '].value();';
-                if (i !== nargs - 1) {
-                    application += ', ';
-                    //getvals += ', ';
-                }
+        var application = '';
+        var getvals = '';//var srcdata = [';
+        for (var i = 0; i < nargs; i++) {
+            //application += 'srcdata[' + i + '][i]';
+            application += 'srcdata_' + i + '[i]';
+            //getvals += 'src[' + i + '].value()';
+            getvals += 'var srcdata_' + i + ' = src[' + i + '].value();';
+            if (i !== nargs - 1) {
+                application += ', ';
+                //getvals += ', ';
             }
-            //getvals += '];\n';
-            console.log(getvals + 
-                'for (var i = 0; i < len; i++)\n' + 
-                '  target[i] = fn(' + application + ');');
-            nListMap.memo[nargs] = new Function('fn', 'src', 'target', 'len', 
-                getvals + 
-                'for (var i = 0; i < len; i++)\n' + 
-                '  target[i] = fn(' + application + ');');            
         }
-        return nListMap.memo[nargs];
+        //getvals += '];\n';
+        console.log(getvals + 
+            'for (var i = 0; i < len; i++)\n' + 
+            '  target[i] = fn(' + application + ');');
+        return new Function('fn', 'src', 'target', 'len', 
+            getvals + 
+            'for (var i = 0; i < len; i++)\n' + 
+            '  target[i] = fn(' + application + ');');
     };
-        
-    nListMap.memo = [];
     
     
     grafar.wrapFn = wrapFn;
     grafar.uniformFn = uniformFn;
-    //grafar.inlineWrap = inlineWrap;
     grafar.nListMap = nListMap;
 }(this));
--- src/core/panel.js
+++ src/core/panel.js
@@ -70,70 +70,59 @@
 	};
 
 	Panel.prototype.drawAxes = function (len) {
-		if (!isExisty(this.axisObject)) {
-			this.axisObject = new THREE.Object3D();
-						
+		if (!isExisty(this.axisObject)) {				
 			var axisGeometry = new THREE.BufferGeometry();
 			axisGeometry.addAttribute('position', new THREE.BufferAttribute(pool.get(Float32Array, 18), 3));
-			this.axisObject.add(new THREE.Line(
+			this.axisObject = new THREE.Line(
 				axisGeometry, 
 				new THREE.LineBasicMaterial({color: 0x888888}), 
 				THREE.LinePieces
-			));
-			
-			for (var i = 0; i < 3; i++) {
-				var geometry = new THREE.BufferGeometry();
-				geometry.addAttribute('position', new THREE.BufferAttribute(axisGeometry.getAttribute('position').array.subarray(i * 6 + 3, i * 6 + 6), 3));
-				this.axisObject.add(new THREE.PointCloud(geometry, new THREE.PointCloudMaterial()));
-			}
-			
+			);			
 			this.scene.add(this.axisObject);
-		}		
+		}
+		setAxisGeometry(this.axisObject.geometry.getAttribute('position').array, len, this._axes.length);
 		
-		if (isExisty(len))
-			setAxisGeometry(this.axisObject.children[0].geometry.getAttribute('position').array, len);
-		this._axes.forEach(function(axisId, i) {
-			drawTextLabel(this.axisObject.children[i + 1].material, axisId || '');
+        if (!isExisty(this.axisLabels)) {
+			this.axisLabels = new THREE.Object3D();
+			for (var i = 0; i < 3; i++) {
+				var labelPos = new THREE.BufferGeometry();
+				labelPos.addAttribute('position', new THREE.BufferAttribute(this.axisObject.geometry.getAttribute('position').array.subarray(i * 6 + 3, i * 6 + 6), 3));
+				this.axisLabels.add(new THREE.PointCloud(labelPos, new THREE.PointCloudMaterial()));
+			}
+            this.scene.add(this.axisLabels);
+        }
+        this.axisLabels.children.forEach(function(child, i) {
+			drawTextLabel(child.material, this._axes[i] || '');
 		}.bind(this));
 		
 		return this;
 	};
 		
-	Panel.prototype.setAxes = function(axisNames) {
-		axisNames = axisNames.filter(function(n) {
-				return typeof(n) === 'string';
-			})
-			.slice(0, 3);
-		
-		this._axes = [axisNames[1], axisNames[2], axisNames[0]];
+	Panel.prototype.setAxes = function(axisNames) {		
+		this._axes = [axisNames[1], axisNames[2], axisNames[0]].filter(isExisty);
 		if (axisNames.length === 3) {
 			this.controls.noRotate = false;
-			this.controls.noPan = false;
 			this.camera.up.set(0, 1, 0);
 		} else if (axisNames.length === 2) {
 			this.controls.noRotate = true;
-			this.controls.noPan = true;
-			this.camera.position.set(0, 5); // preserve distance or something, maybe smooth rotation
+			this.camera.position.set(0, 0, -5);
 			this.camera.up.set(1, 0, 0);
 		} else {
-			// 1 or >3 axes leads to what?
-		}
-			
+            throw new Error('wrong amount of axes specified');
+		}			
 		this.drawAxes(2);
-		// Object.getOwnPropertyNames(_G.graphs).forEach(function(graphName) {
-			// var graph = _G.graphs[graphName];
-			// if (graph.panel === this)
-				// graph.setPanel(this);
-		// }.bind(this));
 		
 		return this;
 	};
 		
 	
-	function setAxisGeometry(posArray, length) {
+	function setAxisGeometry(posArray, length, dim) {
+        console.log(dim)
+        dim = dim || 3;
 		for (var i = 0; i < 3; i++) {
-			posArray[7 * i] = -length;
-			posArray[7 * i + 3] = length;
+            var len = i < dim? length: 0;
+			posArray[7 * i] = -len;
+			posArray[7 * i + 3] = len;
 		}
 		return posArray;
 	}
