8098e703d52a859580d0028dab28d8517c5cd30d
thoughtspile <v.klepov@gmail.com>
2015-09-15 22:42:12 +0300

    Edge topology integration test

--- src/core/non_reactive_graph_stub.js
+++ src/core/non_reactive_graph_stub.js
@@ -7,11 +7,13 @@
     var InstanceGL = grafar.InstanceGL;
     var interleave = grafar.interleave;
 
-    var display = function(panel, coords) {
+    var display = function(panel, coords, opts) {
         var instance = new InstanceGL(panel, {r:Math.random(),g:1,b:1});
         var tab = [coords[1], coords[2], coords[0]];
         interleave(tab, instance.position, 3);
-        instance.object.children[0].visible = true;
+		if (opts && opts.edges)
+			interleave([opts.edges], instance.segments);
+        instance.object.children[0].visible = false;
     };
 
 
