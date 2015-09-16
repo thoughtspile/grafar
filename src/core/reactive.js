ec64a3810142d9252c33e4c83f6009afc9fe004a
thoughtspile <v.klepov@gmail.com>
2015-09-16 10:28:59 +0300

    Reactive is function

--- src/core/reactive.js
+++ src/core/reactive.js
@@ -3,20 +3,30 @@
     var grafar = global.grafar;
     // from misc.js
     var isExisty = grafar.isExisty;
-    
-   
+
+
 	var Reactive = function(data) {
-        this.sources = [];        
-		this.data = isExisty(data)? data: {};
-		this.fn = function() {};
-		this._isValid = false;
+        var self = function() { return self.value(); };
+        self.sources = [];
+		self.data = isExisty(data)? data: {};
+		self.fn = function() {};
+		self._isValid = false;
+
+        Object.keys(Reactive.prototype).forEach(function(prop) {
+            self[prop] = Reactive.prototype[prop];
+        });
+
+        return self;
 	};
-	
+
 	Reactive.isReactive = function(obj) {
-		return obj instanceof Reactive;
+		return Boolean(obj) && Object.keys(Reactive.prototype)
+            .every(function(prop) {
+                return obj[prop] === Reactive.prototype[prop];
+            });
 	};
-    
-    
+
+
     Reactive.prototype.isValid = function() {
         return this._isValid && this.sources.reduce(function(state, src) {
             return state && src.isValid();
@@ -28,19 +38,19 @@
         this.invalidate();
         return this;
     };
-    
-	Reactive.prototype.bind = function(newArgs) {        
+
+	Reactive.prototype.bind = function(newArgs) {
         this.unbind();
         this.sources = newArgs.slice();
         return this;
     };
-    
+
     Reactive.prototype.unbind = function() {
         this.sources.length = 0;
         this.invalidate();
         return this;
     };
-    
+
 	Reactive.prototype.validate = function() {
 		if (!this.isValid()) {
             var res = this.fn(this.sources.map(function(src) {
@@ -52,16 +62,16 @@
 		}
 		return this;
 	};
-	
+
 	Reactive.prototype.invalidate = function() {
 		this._isValid = false;
 		return this;
 	};
-    
+
     Reactive.prototype.value = function() {
         return this.validate().data;
     };
-    
-    
+
+
 	grafar.Reactive = Reactive;
 }(this));
