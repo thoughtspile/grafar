9f145bbfbcec707f461c474589159535ca0d17fd
thoughtspile <v.klepov@gmail.com>
2015-09-24 22:40:43 +0300

    range now accepts cached buffer

--- src/core/buffer_blocks.js
+++ src/core/buffer_blocks.js
@@ -4,14 +4,17 @@
     // buffer
     var Buffer = grafar.Buffer2;
 
-    var range = function(a, b, n) {
-        var buff = new grafar.Buffer2(Float32Array).resize(n);
+    var range = function(a, b, n, buff) {
+        buff = buff || new grafar.Buffer2(Float32Array);
+        buff.resize(n);
+
         var step = (b - a) / n;
         for (var i = 0; i < n; i++)
             buff.array[i] = a + i * step;
+            
         return buff;
     };
-    
+
 
     grafar.range = range;
 }(this));
