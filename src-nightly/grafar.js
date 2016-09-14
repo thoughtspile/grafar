e15c85fcaa28cd47fe69404e5c0186e02ba68fb8
Vladimir Klepov <v.klepov@gmail.com>
2016-09-14 17:39:10 +0300

    purge runGrafar

--- src-nightly/grafar.js
+++ src-nightly/grafar.js
@@ -1,6 +1,7 @@
 import * as THREE from '../libs/three.min';
 import Detector from '../libs/Detector';
 import * as Stats from '../libs/stats.min';
+import * as Color from '../libs/i-color.min';
 import { OrbitControls } from '../libs/OrbitControls'; // FIXME non-standard
 
 // FIXME duct-tape
