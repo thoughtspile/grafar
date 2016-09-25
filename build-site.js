e7c66779b09b107d1ee5d00f2ba821b2dcaa43cc
Vladimir Klepov <v.klepov@gmail.com>
2016-09-25 14:25:12 +0300

    example index generator

---
+++ build-site.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var Handlebars = require('handlebars');
+
+var exampleDir = __dirname + '/examples';
+var template = Handlebars.compile('<html><body><ul>' +
+    '{{#each examples}}<li><a href="{{ . }}">{{ . }}</a></li>{{/each}}' +
+    '</ul></body></html>');
+
+fs.readdir(exampleDir, function(err, files) {
+    var absLinks = files
+        .filter(name => name.match('\.html$'))
+        .map(name => '/examples/' + name);
+    var index = template({ examples: absLinks });
+    fs.writeFileSync(__dirname + '/index.html', index)
+});
