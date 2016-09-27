4a569468954dea99022fccf95d78bbfd9fab6820
Vladimir Klepov <v.klepov@gmail.com>
2016-09-27 15:26:01 +0300

    group cols in nas

--- examples/examples-nas/examples-nas/main_example/derivative.js
+++ examples/examples-nas/examples-nas/main_example/derivative.js
@@ -41,7 +41,10 @@
 	pan3d_main.setAxes(['x', 'y', 'z']);
 
 	var r_1, r_2, С;
-    var dynamicSphere;
+
+    // параметризация сферы
+    var phi = grafar.range(0, 2 * Math.PI, 25);
+    var theta = grafar.range(0, Math.PI, 25);
 
 	updateProblem();
 
@@ -62,48 +65,50 @@
 		document.getElementById('cent').value =  C;
 
 		// Первый шарик
-		var phi = grafar.range(0, 2 * Math.PI, 25);
-    	var theta = grafar.range(0, Math.PI, 25, 'theta');
+    	dynamicSphere = [
+            grafar.map([phi, theta], (phi, theta) => obj_1_x + r_1 * Math.sin(theta) * Math.cos(phi)),
+            grafar.map([phi, theta], (phi, theta) => r_1 * Math.sin(theta) * Math.sin(phi)),
+            grafar.map([phi, theta], (phi, theta) => r_1 * Math.cos(theta))
+        ];
+        grafar.pin(dynamicSphere, pan3d_main);
 
-    	var x1 = grafar.map([phi, theta], (phi, theta) => obj_1_x + r_1 * Math.sin(theta) * Math.cos(phi));
-    	var y1 = grafar.map([phi, theta], (phi, theta) => r_1 * Math.sin(theta) * Math.sin(phi));
-    	var z1 = grafar.map([phi, theta], (phi, theta) => r_1 * Math.cos(theta));
-
-        dynamicSphere = [x1, y1, z1];
-
-        grafar.pin([x1, y1, z1], pan3d_main);
-
-		var c1_x = grafar.constant(obj_1_x);
-		var c1_y = grafar.constant(0);
-		var c1_z = grafar.constant(0);
-        grafar.pin([c1_x, c1_y, c1_z], pan3d_main);
+		var c1 = [
+            grafar.constant(obj_1_x),
+    	    grafar.constant(0),
+    		grafar.constant(0)
+        ];
+        grafar.pin(c1, pan3d_main);
 
 		// Второй шарик
-		var phi2 = grafar.range(0, 2 * Math.PI, 25);
-    	var theta2 = grafar.range(0, Math.PI, 25);
+    	var staticSphere = [
+            grafar.map([phi, theta], (phi, theta) => obj_2_x + r_2 * Math.sin(theta) * Math.cos(phi)),
+            grafar.map([phi, theta], (phi, theta) => r_2 * Math.sin(theta) * Math.sin(phi)),
+    	    grafar.map([phi, theta], (phi, theta) => r_2 * Math.cos(theta))
+        ];
+        grafar.pin(staticSphere, pan3d_main);
 
-    	var x2 = grafar.map([phi, theta], (phi, theta) => obj_2_x + r_2 * Math.sin(theta) * Math.cos(phi));
-    	var y2 = grafar.map([phi, theta], (phi, theta) => r_2 * Math.sin(theta) * Math.sin(phi));
-    	var z2 = grafar.map([phi, theta], (phi, theta) => r_2 * Math.cos(theta));
-
-        grafar.pin([x2, y2, z2], pan3d_main);
-
-		var c2_x = grafar.constant(obj_1_x);
-		var c2_y = grafar.constant(0);
-		var c2_z = grafar.constant(0);
-        grafar.pin([c2_x, c2_y, c2_z], pan3d_main);
+		var c2 = [
+            grafar.constant(obj_1_x),
+    		grafar.constant(0),
+    		grafar.constant(0)
+        ];
+        grafar.pin(c2, pan3d_main);
 
 		// Ось
-		var line_x = grafar.seq(-10, 10, 100);
-    	var line_y = grafar.constant(0, 'y');
-    	var line_z = grafar.constant(0, 'z');
-        grafar.pin([line_x, line_y, line_z], pan3d_main);
+		var line = [
+            grafar.seq(-10, 10, 100),
+        	grafar.constant(0),
+        	grafar.constant(0)
+        ];
+        grafar.pin(line, pan3d_main);
 
 		// Центр масс
-        var cg_x = grafar.constant(C);
-		var cg_y = grafar.constant(0);
-		var cg_z = grafar.constant(0);
-        grafar.pin([cg_x, cg_y, cg_z], pan3d_main);
+        var cg = [
+            grafar.constant(C),
+    		grafar.constant(0),
+    		grafar.constant(0)
+        ];
+        grafar.pin(cg, pan3d_main);
 	}
 
     function prepareAnimation() {
@@ -115,21 +120,19 @@
 
     	C = (m_1 * obj_1_x + m_2 * obj_2_x) / (m_1 + m_2);
 
-        var phi_ref = grafar.range(0, 2 * Math.PI, 25);
-        var theta_ref = grafar.range(0, Math.PI, 25);
         var temp = grafar.constant(0);
-        grafar.constrain({what: dynamicSphere, using: [phi_ref, theta_ref, temp], as: (data, l) => {
+        grafar.constrain({ what: dynamicSphere, using: [phi, theta, temp], as: (data, l) => {
             var deg = data[temp][0] * 0.0174533;
             var track = get_track(C, obj_1_x);
             var obj_1_mass_x = C + track * Math.cos(deg);
             var obj_1_mass_y = track * Math.sin(deg);
 
-            var phi = data[phi_ref];
-            var theta = data[theta_ref];
+            var phi_ = data[phi];
+            var theta_ = data[theta];
             for (var i = 0; i < l; i++) {
-                data[dynamicSphere[0]][i] = obj_1_mass_x + r_1 * Math.sin(theta[i]) * Math.cos(phi[i]);
-                data[dynamicSphere[1]][i] = obj_1_mass_y + r_1 * Math.sin(theta[i]) * Math.sin(phi[i]);
-                data[dynamicSphere[2]][i] = r_1 * Math.cos(theta[i]);
+                data[dynamicSphere[0]][i] = obj_1_mass_x + r_1 * Math.sin(theta_[i]) * Math.cos(phi_[i]);
+                data[dynamicSphere[1]][i] = obj_1_mass_y + r_1 * Math.sin(theta_[i]) * Math.sin(phi_[i]);
+                data[dynamicSphere[2]][i] = r_1 * Math.cos(theta_[i]);
             }
         }});
 
