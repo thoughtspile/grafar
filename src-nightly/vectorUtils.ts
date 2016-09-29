0c5e0db4c36942ef9633f4628d0fc0d3a4612181
Vladimir Klepov <v.klepov@gmail.com>
2016-09-29 16:06:34 +0300

    document vec

--- src-nightly/vectorUtils.ts
+++ src-nightly/vectorUtils.ts
@@ -1,45 +1,56 @@
-function zeroVector(len) {
-    var zero = [];
-    for (var i = 0; i < len; i++)
-        zero[i] = 0;
-    return zero;
-}
-
-function dot(a, b) {
-    var temp = 0,
-        l = a.length;
-    for (var i = 0; i < l; i++)
+/**
+ * dot - скалярное произведение <a, b>
+ */
+function dot(a: ArrayLike<number>, b: ArrayLike<number>) {
+    let temp = 0;
+    const l = Math.min(a.length, b.length);
+    for (var i = 0; i < l; i++) {
         temp += a[i] * b[i];
+    }
     return temp;
 }
 
-function norm(a) {
-    var aNorm = 0,
-        l = a.length;
-    for (var i = 0; i < l; i++)
+/**
+ * L1-норма вектора a (сумма модулей компонент).
+ * L1, а не нормальная норма, потому что быстрее, но, возможно, это херня.
+ * Нормальная Евклидова норма -- в norm2
+ */
+function norm(a: ArrayLike<number>) {
+    let aNorm = 0;
+    const l = a.length;
+    for (var i = 0; i < l; i++) {
         aNorm += Math.abs(a[i]);
+    }
     return aNorm;
 }
 
-function norm2(a) {
-    var aNorm2 = 0,
-        l = a.length;
-    for (var i = 0; i < l; i++)
+/**
+ * Норма (нормальная, Евклидова, L2-норма) вектора a.
+ * L1-норма -- в norm
+ */
+function norm2(a: ArrayLike<number>) {
+    let aNorm2 = 0;
+    const l = a.length;
+    for (var i = 0; i < l; i++) {
         aNorm2 += a[i] * a[i];
-    aNorm2 = Math.sqrt(aNorm2);
-    return aNorm2;
+    }
+    return Math.sqrt(aNorm2);
 }
 
+/**
+ * L1-расстояние между векторами a, b.
+ * TODO: проверить, не смешиваются ли где-нибудь L1 и L2, неловко получится.
+ */
 function dist(a, b) {
-    var abDist = 0,
-        l = a.length;
-    for (var i = 0; i < l; i++)
+    let abDist = 0;
+    const l = a.length;
+    for (var i = 0; i < l; i++) {
         abDist += Math.abs(a[i] - b[i]);
+    }
     return abDist;
 }
 
 export {
-    zeroVector,
     dot,
     norm,
     norm2,
