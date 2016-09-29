ecda6b3cc45a9197c0b27a7ba99a6bc51f55f68a
Vladimir Klepov <v.klepov@gmail.com>
2016-09-29 16:19:05 +0300

    more docs

--- src-nightly/utils.ts
+++ src-nightly/utils.ts
@@ -1,4 +1,9 @@
-function makeID(obj) {
+/**
+ * Возвращает ключ, которого еще нет в obj.
+ * Например, чтобы генерировать случайные уникальные имена переменных.
+ * Формат ключа: '__grafar' + около 7 случайных символов.
+ */
+function makeID(obj: { [key: string]: any }) {
     while (true) {
         var temp = '__grafar' + Math.random().toString(36).substr(2, 9);
         if (!(temp in obj))
@@ -6,15 +11,25 @@ function makeID(obj) {
     }
 }
 
-function isExisty(obj) {
+/**
+ * obj -- не undefined и не null.
+ * Имеет смысл использовать только для атомарных типов: объект или есть, или фолси, смело делайте if (obj) или obj || {}.
+ * Также используйте ES6-параметры-по-умолчанию: (x: number = 2)
+ */
+function isExisty(obj: any) {
     return typeof(obj) !== 'undefined' && obj !== null;
 }
 
-function asArray(str) {
-    if (typeof str === 'string')
+/**
+ * Если передать строку, то почистит ее от пробелов и разобъет по `,`
+ * Если передать массив, вернет его же, но без null и undefined.
+ * Функция нормализовывала имена переменных, чтобы 'x,y', 'x, y' и ['x', 'y'] становились одинаковыми, но теперь, может, уже и не актуально.
+ */
+function asArray(str: string | string[]) {
+    if (typeof str === 'string') {
         return str.replace(/ /g, '').split(',');
-    else
-        return str.filter(isExisty);
+    }
+    return str.filter(isExisty);
 }
 
 
--- src-nightly/vectorUtils.ts
+++ src-nightly/vectorUtils.ts
@@ -4,7 +4,7 @@
 function dot(a: ArrayLike<number>, b: ArrayLike<number>) {
     let temp = 0;
     const l = Math.min(a.length, b.length);
-    for (var i = 0; i < l; i++) {
+    for (let i = 0; i < l; i++) {
         temp += a[i] * b[i];
     }
     return temp;
@@ -18,7 +18,7 @@ function dot(a: ArrayLike<number>, b: ArrayLike<number>) {
 function norm(a: ArrayLike<number>) {
     let aNorm = 0;
     const l = a.length;
-    for (var i = 0; i < l; i++) {
+    for (let i = 0; i < l; i++) {
         aNorm += Math.abs(a[i]);
     }
     return aNorm;
@@ -31,7 +31,7 @@ function norm(a: ArrayLike<number>) {
 function norm2(a: ArrayLike<number>) {
     let aNorm2 = 0;
     const l = a.length;
-    for (var i = 0; i < l; i++) {
+    for (let i = 0; i < l; i++) {
         aNorm2 += a[i] * a[i];
     }
     return Math.sqrt(aNorm2);
@@ -41,10 +41,10 @@ function norm2(a: ArrayLike<number>) {
  * L1-расстояние между векторами a, b.
  * TODO: проверить, не смешиваются ли где-нибудь L1 и L2, неловко получится.
  */
-function dist(a, b) {
+function dist(a: ArrayLike<number>, b: ArrayLike<number>) {
     let abDist = 0;
-    const l = a.length;
-    for (var i = 0; i < l; i++) {
+    const l = Math.min(a.length, b.length);
+    for (let i = 0; i < l; i++) {
         abDist += Math.abs(a[i] - b[i]);
     }
     return abDist;
