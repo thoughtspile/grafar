43a5a5b215bddd6eaa8c9332f12f3e325ef6a3ce
Vladimir Klepov <v.klepov@gmail.com>
2016-10-01 18:23:55 +0300

    mv base-related functionnality into TopoRegistry

--- src-nightly/Graph.ts
+++ src-nightly/Graph.ts
@@ -6,6 +6,23 @@ import { Reactive } from './Reactive';
 import * as _ from 'lodash';
 import { makeID } from './utils';
 
+export class TopoRegistry {
+    static free(edges, data) {
+        return new Reactive([{ id: makeID(), edges: null, length: 0 }])
+            .lift(([ edges, data ], self) => {
+                self[0].edges = edges;
+                self[0].length = data.length;
+            })
+            .bind([ edges, data ]);
+    }
+
+    static derive(bases) {
+        return new Reactive([])
+            .lift(src => _.union.apply(_, src).sort((a, b) => a.id.localeCompare(b.id)))
+            .bind(bases);
+    }
+}
+
 export class Graph {
     constructor() {}
 
@@ -13,12 +30,7 @@ export class Graph {
     edges = new Reactive<GraphBuffer>({ array: new Uint32Array(0), length: 0, pointCount: 0 });
     faces = new Reactive<GraphBuffer>({ array: new Uint32Array(0), length: 0, pointCount: 0 });
     colors = new Reactive<Buffer>({ array: new Float32Array(0), length: 0 });
-    base = new Reactive([{ id: makeID(), edges: null, length: 0 }])
-        .lift(([ edges, data ], self) => {
-            self[0].edges = edges;
-            self[0].length = data.length;
-        })
-        .bind([ this.edges, this.data ]);
+    base = TopoRegistry.free(this.edges, this.data);
 
     static contextify(col, targetBase) {
         const temp = new Graph();
@@ -55,9 +67,7 @@ export class Graph {
     }
 
     static unify(cols: Graph[]) {
-        const targetBase = new Reactive([])
-            .lift(src => _.union.apply(_, src).sort((a, b) => a.id.localeCompare(b.id)))
-            .bind(cols.map(col => col.base));
+        const targetBase = TopoRegistry.derive(cols.map(col => col.base);
 
         const baseEdges = new Reactive([])
             .lift(([ bases ], targ) => bases.map(base => base.edges))
