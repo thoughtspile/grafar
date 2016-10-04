c725ef2a605a29cbfde1be76bd6989e5ce5f08f9
Vladimir Klepov <v.klepov@gmail.com>
2016-10-04 11:53:17 +0300

    rm alt sources

--- src-v3/demo--canvas.js
+++
@@ -1,7 +0,0 @@
-import * as grafar from './grafar';
-
-const x = grafar.range(0, 1, 10);
-console.log(x().raw());
-
-const y = grafar.map(x, x => 2 * x);
-console.log(y().raw());
--- src/blocks/Block.js
+++
@@ -1,9 +0,0 @@
-import { Buffer } from './buffer';
-
-export class Block {
-    constructor(size) {
-        this.size = size;
-        this.data = new Buffer(Float32Array).resize(size);
-        this.topo = null;
-    }
-}
