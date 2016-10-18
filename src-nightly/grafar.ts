c57be6fa4be0802c761264c663acd54a05c3d1ce
Vladimir Klepov <v.klepov@gmail.com>
2016-10-18 13:07:52 +0300

    expose grafar props

--- src-nightly/grafar.ts
+++ src-nightly/grafar.ts
@@ -1,98 +1,98 @@
-// import * as _ from 'lodash';
-//
-// import { UI } from './UI';
-//
-// import { config } from './config';
-// import { registry } from './registry';
-// import { Style } from './Style';
-// import { Panel, panels } from './Panel';
-// import { GrafarObject } from './GrafarObject';
-// import * as generators from './generators';
-// import { makeID, asArray } from './utils';
-// import { Pin } from './Pin';
-//
-// const normalizeNames = (names: string[] | string[][], forceDim?: number) => {
-//     const flatVars = _.flatten(names);
-//     return forceDim? _.range(forceDim).map(i => flatVars[i] || null): flatVars;
-// }
+import * as _ from 'lodash';
+
+import { UI } from './UI';
+
+import { config } from './config';
+import { registry } from './registry';
+import { Style } from './Style';
+import { Panel, panels } from './Panel';
+import { GrafarObject } from './GrafarObject';
+import * as generators from './generators';
+import { makeID, asArray } from './utils';
+import { Pin } from './Pin';
+
+const normalizeNames = (names: string[] | string[][], forceDim?: number) => {
+    const flatVars = _.flatten(names);
+    return forceDim? _.range(forceDim).map(i => flatVars[i] || null): flatVars;
+}
 
 const grafar = {
     version: '4.01r',
 
-    // update() {
-    //     panels.forEach(panel => panel.update());
-    //     grafar.frameId++;
-    //     window.requestAnimationFrame(grafar.update);
-    // },
-    //
-    // setup(changes, target) {
-    //     target = target || config;
-    //     Object.keys(changes).forEach(name => {
-    //         if (!target.hasOwnProperty(name)) {
-    //             return;
-    //         }
-    //         if (name === 'grafaryaz') {
-    //             grafar.setup(changes[name], config.grafaryaz);
-    //             return;
-    //         }
-    //         target[name] = changes[name];
-    //     });
-    //     return grafar;
-    // },
-    //
-    // config,
-    // panels,
-    // Style,
-    // Panel,
-    // generators,
-    //
-    // UI,
-    // ui(mockup, opts) {
-    //     opts = opts || {};
-    //     var container = opts.container || document;
-    //     if (typeof(container) === 'string')
-    //         container = document.getElementById(container);
-    //     if (mockup instanceof Array)
-    //         mockup = {init: mockup, type: 'group'};
-    //
-    //     UI.push(mockup, container);
-    //
-    //     return this;
-    // },
-    //
-    // map(using, fn) {
-    //     const uid = makeID(Object.keys(registry.datasets));
-    //     return registry.map(uid, using, fn);
-    // },
-    //
-    // constrain: registry.constrain.bind(registry),
-    //
-    // refresh: () => Pin.refresh(),
-    //
-    // // pin(vars: { axes: string[][] | string[]; color: string[][] | string[]}, panel: Panel)
-    // // pin(vars: string[][] | string[], panel)
-    // pin(vars: any, panel) {
-    //     const props = {
-    //         axes: normalizeNames(vars.axes || vars, 3),
-    //         color: vars.color? normalizeNames(vars.color, 3): null
-    //     };
-    //     const pin = new Pin(props, panel);
-    //     pin.refresh();
-    // },
-    //
-    // frameId: 0,
-    //
-    // // real generators: explicit transfer to preserve typings
-    // set: generators.set,
-    // constant: generators.constant,
-    // ints: generators.ints,
-    // seq: generators.seq,
-    // range: generators.range,
-    // logseq: generators.logseq,
-    // vsolve: generators.vsolve
+    update() {
+        panels.forEach(panel => panel.update());
+        grafar.frameId++;
+        window.requestAnimationFrame(grafar.update);
+    },
+
+    setup(changes, target) {
+        target = target || config;
+        Object.keys(changes).forEach(name => {
+            if (!target.hasOwnProperty(name)) {
+                return;
+            }
+            if (name === 'grafaryaz') {
+                grafar.setup(changes[name], config.grafaryaz);
+                return;
+            }
+            target[name] = changes[name];
+        });
+        return grafar;
+    },
+
+    config,
+    panels,
+    Style,
+    Panel,
+    generators,
+
+    UI,
+    ui(mockup, opts) {
+        opts = opts || {};
+        var container = opts.container || document;
+        if (typeof(container) === 'string')
+            container = document.getElementById(container);
+        if (mockup instanceof Array)
+            mockup = {init: mockup, type: 'group'};
+
+        UI.push(mockup, container);
+
+        return this;
+    },
+
+    map(using, fn) {
+        const uid = makeID(Object.keys(registry.datasets));
+        return registry.map(uid, using, fn);
+    },
+
+    constrain: registry.constrain.bind(registry),
+
+    refresh: () => Pin.refresh(),
+
+    // pin(vars: { axes: string[][] | string[]; color: string[][] | string[]}, panel: Panel)
+    // pin(vars: string[][] | string[], panel)
+    pin(vars: any, panel) {
+        const props = {
+            axes: normalizeNames(vars.axes || vars, 3),
+            color: vars.color? normalizeNames(vars.color, 3): null
+        };
+        const pin = new Pin(props, panel);
+        pin.refresh();
+    },
+
+    frameId: 0,
+
+    // real generators: explicit transfer to preserve typings
+    set: generators.set,
+    constant: generators.constant,
+    ints: generators.ints,
+    seq: generators.seq,
+    range: generators.range,
+    logseq: generators.logseq,
+    vsolve: generators.vsolve
 };
 
 // bootstrap
-// grafar.update();
+grafar.update();
 
 export = grafar;
