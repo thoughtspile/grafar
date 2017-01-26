bf948c4c86ad5366df3433b2b9e1f9efcbcb1787
Vladimir Klepov <v.klepov@gmail.com>
2017-01-26 12:35:31 +0300

    comment on Generator

--- src/Generator.ts
+++ src/Generator.ts
@@ -4,10 +4,18 @@ import { GrafarObject, ConstraintData } from './core/GrafarObject';
 import { isExisty, makeID } from './utils';
 import { registry } from './core/registry';
 
+/**
+ * Генератор - не-материализованное описание измерения.
+ * Можно сделать его новой графар-переменной (.select()),
+ * можно обновить старую (.into(target));
+ */
 export class Generator {
     constructor(private anonymousConstraint: ConstraintData) {
     }
 
+    /**
+     * Материализовать генератор
+     */
     select() {
         const names = _.range(this.getDimension())
             .map(() => makeID());
@@ -15,6 +23,11 @@ export class Generator {
         return registry.extern(this.anonymousConstraint);
     }
 
+    /**
+     * Почтему source.into(target), а не target.set(source)?
+     * Чтобы иметь возможность группировать target через простой массив
+     * Особенно это касается n-мерных генераторов: grafar.vsolve(...).into([x, y, z]);
+     */
     into(names: string[]) {
         /** TODO normalize names */
         const expectDim = this.getDimension();
